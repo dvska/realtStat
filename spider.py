76d77422a90f3c1382486ea3bb5fa4592618e77d
bychkovav <aliaksei.bychkou@gmail.com>
2017-07-02 18:23:14 +0300

    fix some errors

--- spider.py
+++ spider.py
@@ -7,26 +7,28 @@ import re
 class RealtSpider(scrapy.Spider):
     name = "list"
     count = 0
+    data = {"items": []}
 
     start_urls = [
         'https://realt.by/sale/flats/',
     ]
 
     def parse(self, response):
-        file = open('results.json', 'w', encoding='utf-16')
-        data = {"items": []}
+        items = response.css('div.bd-item')
 
-        for item in response.css('div.bd-item'):
+        for item in items:
             price = re.findall('(\d+\s*\d*)', html.unescape(item.css('span.price-byr::text').extract_first()))
             if len(price) > 0:
                 price = price[0]
             name = item.css('div.title a::text').extract_first()
-            data["items"].append({"name": name, "price": price})
-
-        file.write(json.dumps(data, ensure_ascii=False, indent=4))
-        file.close()
+            self.data["items"].append({"name": name, "price": price})
 
         next_page = response.css('div.uni-paging a.active + a::attr("href")').extract_first()
         if next_page is not None and self.count < 10:
             self.count += 1
             yield response.follow(next_page, self.parse)
+        else:
+            self.data["count"] = len(self.data["items"])
+            file = open('results.json', 'w', encoding='utf-16')
+            file.write(json.dumps(self.data, ensure_ascii=False, indent=4))
+            file.close()
