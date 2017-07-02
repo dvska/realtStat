1e80a0b7573345713a9806b069baa48e030d031e
bychkovav <aliaksei.bychkou@gmail.com>
2017-07-02 13:52:51 +0300

    price added

--- spider.py
+++ spider.py
@@ -1,5 +1,8 @@
 import scrapy
 import json
+import html
+import re
+
 
 class RealtSpider(scrapy.Spider):
     name = "list"
@@ -13,10 +16,14 @@ class RealtSpider(scrapy.Spider):
         file = open('results.json', 'w', encoding='utf-16')
         data = {"items": []}
 
-        for item in response.css('div.bd-item > div.title'):
-            data["items"].append({"name":item.css('a::text').extract_first()})
+        for item in response.css('div.bd-item'):
+            price = re.findall('(\d+\s*\d*)', html.unescape(item.css('span.price-byr::text').extract_first()))
+            if len(price) > 0:
+                price = price[0]
+            name = item.css('div.title a::text').extract_first()
+            data["items"].append({"name": name, "price": price})
 
-        file.write(json.dumps(data, ensure_ascii=False))
+        file.write(json.dumps(data, ensure_ascii=False, indent=4))
         file.close()
 
         next_page = response.css('div.uni-paging a.active + a::attr("href")').extract_first()
