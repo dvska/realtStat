0ae7a47beae0f815727c2fe5813833a489ca00cf
bychkovav <aliaksei.bychkou@gmail.com>
2017-07-02 12:03:39 +0300

    moved to json

--- scraper.py
+++ scraper.py
@@ -3,9 +3,7 @@ from scrapy.crawler import CrawlerProcess
 from spider import RealtSpider
 
 process = CrawlerProcess({
-    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
-    'FEED_FORMAT': 'json',
-    'FEED_URI': 'result.json'
+    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
 })
 
 process.crawl(RealtSpider)
--- spider.py
+++ spider.py
@@ -1,20 +1,25 @@
 import scrapy
-
+import json
 
 class RealtSpider(scrapy.Spider):
     name = "list"
+    count = 0
+
     start_urls = [
         'https://realt.by/sale/flats/',
     ]
 
     def parse(self, response):
-        file = open('results.txt', 'w', encoding='utf-16')
+        file = open('results.json', 'w', encoding='utf-16')
+        data = {"items": []}
+
         for item in response.css('div.bd-item > div.title'):
-            i = item.css('a::text').extract_first()
-            file.write(i)
+            data["items"].append({"name":item.css('a::text').extract_first()})
+
+        file.write(json.dumps(data, ensure_ascii=False))
         file.close()
 
-
         next_page = response.css('div.uni-paging a.active + a::attr("href")').extract_first()
-        if next_page is not None:
+        if next_page is not None and self.count < 10:
+            self.count += 1
             yield response.follow(next_page, self.parse)
