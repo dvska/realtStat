0aa39932ce98126cae4c950753e65f54493d4a5e
bychkovav <aliaksei.bychkou@gmail.com>
2017-07-03 14:03:52 +0300

    plot

--- scraper.py
+++ scraper.py
@@ -1,12 +1,10 @@
 import json
-import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as  np
-from operator import attrgetter
 from pandas.io.json import json_normalize
 
 from scrapy.crawler import CrawlerProcess
 from spider import RealtSpider
+import matplotlib.pyplot as plt
 
 process = CrawlerProcess({
     'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
@@ -15,9 +13,6 @@ process = CrawlerProcess({
 process.crawl(RealtSpider)
 process.start()
 
-file = open('results.json', 'r', encoding='utf-16')
-obj = json.load(file)
 
-df = json_normalize(obj['items'])
-df.price = df.price.apply(pd.to_numeric)
-print(df.price.min())
+
+
--- spider.py
+++ spider.py
@@ -2,6 +2,9 @@ import scrapy
 import json
 import html
 import re
+import matplotlib.pyplot as plt
+import pandas as pd
+from pandas.io.json import json_normalize
 
 
 class RealtSpider(scrapy.Spider):
@@ -19,14 +22,14 @@ class RealtSpider(scrapy.Spider):
         for item in items:
             price = re.findall('(\d+\s*\d*)', html.unescape(item.css('span.price-byr::text').extract_first()))
             if len(price) > 0:
-                price = re.sub(r"\s+", "", price[0])
+                price = float(re.sub(r"\s+", "", price[0]))
             else:
                 price = None
             name = item.css('div.title a::text').extract_first()
-            self.data["items"].append({"name": name, "price": float(price)})
+            self.data["items"].append({"name": name, "price": price})
 
         next_page = response.css('div.uni-paging a.active + a::attr("href")').extract_first()
-        if next_page is not None and self.count < 1:
+        if next_page is not None and self.count < 10:
             self.count += 1
             yield response.follow(next_page, self.parse)
         else:
@@ -34,3 +37,33 @@ class RealtSpider(scrapy.Spider):
             file = open('results.json', 'w', encoding='utf-16')
             file.write(json.dumps(self.data, ensure_ascii=False, indent=4))
             file.close()
+
+            df = json_normalize(self.data['items'])
+            df.price = df.price.apply(pd.to_numeric)
+
+            mi = df.price.min()
+            ma = df.price.max()
+
+            intervalcount = 6
+
+            delta = int((ma - mi) / intervalcount)
+
+            intervals = []
+
+            for i in range(0, intervalcount - 1):
+                intervals.append((mi - 1, mi + delta))
+                mi += delta
+
+            intervals.append((mi - 1, ma))
+
+            results = []
+            for i in intervals:
+                results.append(len(df[(df.price > i[0]) & (df.price <= i[1])]))
+
+            x = range(0, 6)
+            labels = ['{} to {}'.format(x[0], x[1]) for x in intervals]
+
+            plt.plot(x, results, 'ro')
+            plt.xticks(x, labels, rotation='vertical')
+            plt.margins(0.2)
+            plt.show()
