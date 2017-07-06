68bdfe13eb426e0527b3389399c7ad48101b9dbe
bychkovav <b_alexey@tut.by>
2017-07-06 16:37:11 +0300

    features added

--- datastoragepipeline.py
+++ datastoragepipeline.py
@@ -1,4 +1,5 @@
 import json
+from bson import json_util
 
 class DataStoragePipeline(object):
     def open_spider(self, spider):
@@ -7,7 +8,7 @@ class DataStoragePipeline(object):
 
     def close_spider(self, spider):
         self.data["count"] = len(self.data["items"])
-        self.file.write(json.dumps(self.data, ensure_ascii=False, indent=4))
+        self.file.write(json.dumps(self.data, default=json_util.default, ensure_ascii=False, indent=4))
         self.file.close()
 
     def process_item(self, item, spider):
--- detailedscraper.py
+++ detailedscraper.py
@@ -1,6 +1,7 @@
 import html
 import re
 import scrapy
+from datetime import datetime
 
 
 class DetailedSpider(scrapy.Spider):
@@ -13,15 +14,57 @@ class DetailedSpider(scrapy.Spider):
     ]
 
     def parsedetailed(self, response):
+        obj = {}
         price = re.findall('(\d+\s*\d*)', html.unescape(response.css('span.price-byr::text').extract_first()))
+
         if len(price) > 0:
             price = float(re.sub(r"\s+", "", price[0]))
         else:
             price = None
         city = response.xpath(
             u'//table/tbody/tr/td[text()="Населенный пункт"]/following-sibling::td/a/strong/text()').extract_first()
+        district = response.xpath(
+            u'//table/tbody/tr/td[text()="Область"]/following-sibling::td/a/text()').extract_first()
+        agency = response.xpath(
+            u'//table/tbody/tr/td[text()="Агенство"]').extract_first()
+        date = response.xpath(
+            u'//table/tbody/tr/td[text()="Дата обновления"]/following-sibling::td/text()').extract_first()
+        street = response.xpath(u'//table/tbody/tr/td[text()="Адрес"]/following-sibling::td/a/text()').extract_first()
+        addressline = response.xpath(
+            u'//table/tbody/tr/td[text()="Адрес"]/following-sibling::td/text()').extract_first()
+
+        if street is not None:
+            obj["street"] = street.replace("ул.", "").strip()
+
+        if addressline is not None:
+            addressparts = re.findall('(\d+)', addressline)
+            if len(addressparts) > 0:
+                obj["house"] = addressparts[0]
+            if len(addressparts) > 1:
+                obj["flat"] = addressparts[1]
+
+        if date is not None:
+            date = datetime.strptime(date, '%Y-%M-%d')
+
+        if agency is not None:
+            obj["agency"] = True
+        else:
+            obj["agency"] = False
+
+        if city is not None:
+            cityparts = city.split(".")
+            city = cityparts[len(cityparts) - 1].strip()
+
+        if district is not None:
+            district = district.replace("область", "").strip()
+
         urlparts = response.url.strip("/").split("/")
-        return {'city': city, 'price': price, 'id': int(urlparts[len(urlparts)-1])}
+        obj["city"] = city
+        obj["price"] = price
+        obj["id"] = int(urlparts[len(urlparts) - 1])
+        obj["district"] = district
+        obj["date"] = date
+        return obj
 
     def parse(self, response):
         links = response.css('div.bd-item .title a')
